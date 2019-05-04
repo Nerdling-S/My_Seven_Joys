55819070efb82a7e27ca6410e8366ca74846ca27
Nerdling_S <42704510+NerdlingS@users.noreply.github.com>
2019-05-04 14:17:59 +0200

    Functional Movement and Camera Follow
    Left/Right Keys and Movement
    Revamped to use 'move_and_slide' instead of 'move_and_collide' due to issues with left/right movement
    Following Camera

--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -2,23 +2,30 @@ extends KinematicBody2D
 
 var velocity = Vector2(0,0)
 
-var gravity = 1
-var jump_strength = 120
+var gravity = 750
 
-var midair = false
+var jump_strength = 220
 
-func _unhandled_input(event):
-	if event.is_action("game_jump") && !midair:
+var run_speed = 100
+
+func poll_input():
+	velocity.x = 0
+	if Input.is_action_pressed("game_jump") && is_on_floor():
 		velocity.y = -jump_strength
-		midair = true
+		
+	if Input.is_action_pressed("game_right"):
+		velocity.x += run_speed
+		
+	if Input.is_action_pressed("game_left"):
+		velocity.x -= run_speed
 
 func _physics_process(delta):
+	velocity.y += gravity * delta
 	
-	velocity.y += gravity
+	poll_input()
 	
-	var collision_info = move_and_collide(velocity * delta)
+	var collision_info
 	
-	if collision_info && midair:
-		if collision_info.get_position().y >= position.y:
-			midair = false
-			print(collision_info.collider)
+	collision_info = move_and_collide(velocity * delta)
+	
+	velocity = move_and_slide(velocity, Vector2(0,-1))
