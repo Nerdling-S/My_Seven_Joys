ea0ba3225ab0e39404f83276b5b6f32d24a2d1ae
Nerdling_S <42704510+NerdlingS@users.noreply.github.com>
2019-05-04 12:52:01 +0200

    Base Jump and Gravity Framework
    KinematicBody2D
    Gravity
    Jump Keys and Strength
    Floor Detection

--- characters/player/Player.gd
+++ characters/player/Player.gd
@@ -1,11 +1,24 @@
-extends Sprite
+extends KinematicBody2D
 
+var velocity = Vector2(0,0)
 
+var gravity = 1
+var jump_strength = 120
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass
+var midair = false
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _unhandled_input(event):
+	if event.is_action("game_jump") && !midair:
+		velocity.y = -jump_strength
+		midair = true
+
+func _physics_process(delta):
+	
+	velocity.y += gravity
+	
+	var collision_info = move_and_collide(velocity * delta)
+	
+	if collision_info && midair:
+		if collision_info.get_position().y >= position.y:
+			midair = false
+			print(collision_info.collider)
